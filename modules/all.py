330a558928e664ed11b7bc39ed655e77498a6ad4
pesekon2 <pesej.ondrek@gmail.com>
2018-10-08 17:13:28 +0200

    make relative imports in all.py python3-friendly

--- modules/all.py
+++ modules/all.py
@@ -1,20 +1,20 @@
-from properties import *
-from pjio import *
-from pixops import *
-from ngbops import *
-from geometry import *
-from ccops import *
-from clssfy import *
-from demops import *
+from .properties import *
+from .pjio import *
+from .pixops import *
+from .ngbops import *
+from .geometry import *
+from .ccops import *
+from .clssfy import *
+from .demops import *
 
-from properties import _Properties
-from pjio import _IO
-from pixops import _PixOps
-from ngbops import _NgbOps
-from geometry import _Geometry
-from ccops import _CCOps
-from clssfy import _Classify
-from demops import _DEMOps
+from .properties import _Properties
+from .pjio import _IO
+from .pixops import _PixOps
+from .ngbops import _NgbOps
+from .geometry import _Geometry
+from .ccops import _CCOps
+from .clssfy import _Classify
+from .demops import _DEMOps
 
 
 class _All(_Properties, _IO, _PixOps, _NgbOps, _Geometry, _CCOps, _Classify,
