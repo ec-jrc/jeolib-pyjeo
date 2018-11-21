99905f9fba39c88a886d0b1fdfaff5e6fa425561
pesekon2 <pesej.ondrek@gmail.com>
2018-11-21 17:07:15 +0100

    enhance test suite for stats with getStats()

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -11,13 +11,37 @@ tiles = ['tests/data/red1.tif', 'tests/data/red2.tif']
 class BadStats(unittest.TestCase):
     """Test functions and methods for getting statistics."""
 
+    def test_getStats(self):
+        """Test if values from getStats are not suspicious."""
+        jim = pj.Jim(tiles[0])
+        stats = jim.stats.getStats()
+
+        max = stats['max']
+        min = stats['min']
+        mean = stats['mean']
+
+        jim_min_dict = jim.stats.getStats(function='min')
+
+        for key, val in jim_min_dict.iteritems():
+            assert val == min, 'Error in getting statistics with getStats'
+
+        assert min < mean < max, 'Error in getting statistics with getStats'
+
+        assert jim_min_dict == pj.stats.getStats(jim, function='min')
+
     def test_histograms(self):
         """Test that values of histograms are not suspicious."""
         jim1 = pj.Jim(tiles[0])
 
+        jim1_rows = jim1.properties.nrOfRow()
+        jim1_cols = jim1.properties.nrOfCol()
+
         histo1d = jim1.stats.getHisto1d()
 
         assert histo1d.stats.getStats()['min'] >= 0, \
+            'Error in computing 1D histogram'
+
+        assert histo1d.stats.getStats()['max'] <= jim1_rows * jim1_cols, \
             'Error in computing 1D histogram'
 
         assert histo1d.pixops.isEqual(pj.stats.getHisto1d(jim1)), \
@@ -30,6 +54,9 @@ class BadStats(unittest.TestCase):
         assert histo2d.stats.getStats()['min'] >= 0, \
             'Error in computing 2D histogram'
 
+        assert histo2d.stats.getStats()['max'] <= jim1_rows * jim1_cols, \
+            'Error in computing 2D histogram'
+
         assert histo2d.pixops.isEqual(pj.stats.getHisto2d(jim1, jim2)), \
             'Function and method getHisto2d() return different results'
 
@@ -40,10 +67,6 @@ class BadStats(unittest.TestCase):
         jim = pj.Jim(testFile)
 
         jim_min = jim.stats.getStats()['min']
-        jim_min_dict = jim.stats.getStats(function='min')
-
-        for key, val in jim_min_dict.iteritems():
-            assert jim_min == val, 'Error in getting statistics with getStats'
 
         jim_stretched = pj.stats.stretch(jim, dst_min=20)
 
