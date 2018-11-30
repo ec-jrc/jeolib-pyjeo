8b787b9f051e8b05d6e209c2bdc6db73581e0ba9
pesekon2 <pesej.ondrek@gmail.com>
2018-11-30 17:47:00 +0100

    fix problems with setFunctions in pixops and create test suite

--- pyjeo/modules/pixops.py
+++ pyjeo/modules/pixops.py
@@ -81,6 +81,13 @@ def convert(jim_object, otype, **kwargs):
     return _pj.Jim(jim_object.convert(kwargs))
 
 
+def isEqual(first_jim, second_jim):
+    if isinstance(other, _pj.Jim):
+        return first_jim.isEqual(second_jim)
+    else:
+        return False
+
+
 def NDVI(jim_object, redBand, nirBand):
     """Compute NDVI on the Jim object.
 
@@ -102,6 +109,54 @@ def NDVISeparateBands(redJim, nirJim):
     :return: a Jim object with values of NDVI
     """
     return _pj.Jim(nirJim.pointOpNDI(redJim))
+
+
+def setThreshold(jim_object, **kwargs):
+    """Apply min and max threshold to pixel values in raster dataset.
+
+    :param kwargs: See table below
+
+    Modifies the instance on which the method was called.
+
+
+    +------------------+--------------------------------------------------+
+    | key              | value                                            |
+    +==================+==================================================+
+    | min              | Minimum threshold value (if pixel value < min    |
+    |                  | set pixel value to no data)                      |
+    +------------------+--------------------------------------------------+
+    | max              | Maximum threshold value                          |
+    |                  | (if pixel value < max set pixel value to no data)|
+    +------------------+--------------------------------------------------+
+    | value            | value to be set if within min and max            |
+    |                  | (if not set, valid pixels will remain their input|
+    |                  | value)                                           |
+    +------------------+--------------------------------------------------+
+    | abs              | Set to True to perform threshold test to absolute|
+    |                  | pixel values                                     |
+    +------------------+--------------------------------------------------+
+    | nodata           | Set pixel value to this no data if pixel value   |
+    |                  | < min or > max                                   |
+    +------------------+--------------------------------------------------+
+
+    .. note::
+
+        A simplified interface to set a threshold is provided via the index
+        operator [] (see :ref:`example <setitem_example>` below).
+
+    .. _setitem_example:
+
+    Example:
+
+    Mask all values not within [0,250] and set to 255::
+
+        jim0[(jim0<0) | (jim0>250)]=255
+
+        Mask all values not within [0,250] and set to 255 (no data)::
+
+        jim_threshold=jim.setThreshold(min=0,max=250,nodata=255)
+    """
+    return _pj.Jim(jim_object.setThreshold(kwargs))
 
 
 def supremum(jim, *args):
@@ -131,52 +186,6 @@ class _PixOps():
 
     def _set_caller(self, caller):
         self._jim_object = caller
-
-    def isEqual(self, other):
-        if isinstance(other, _pj.Jim):
-            return self._jim_object.isEqual(other)
-        else:
-            return False
-
-    def setData(self, value, ulx, uly, lrx, lry, bands=0, dx=None, dy=None,
-                geo=True):
-        """Set range of pixels to value.
-
-        :param jim_object: a Jim object
-        :param value: new value for pixels of Jim object
-        :param ulx: upper left corner x coordinate (in projected coordinates
-            if geo is True, else in image coordinates)
-        :param uly: upper left corner y coordinate (in projected coordinates
-            if geo is True, else in image coordinates)
-        :param lrx: lower right corner x coordinate (in projected coordinates
-            if geo is True, else in image coordinates)
-        :param lry: lower right corner y coordinate (in projected coordinates
-            if geo is True, else in image coordinates)
-        :param bands: List of band indices to crop (index is 0 based)
-        :param dx: spatial resolution in x to crop (stride if geo is False)
-        :param dy: spatial resolution in y to crop (stride if geo is False)
-        :param geo: use geospatial coordinates if True, image coordinates
-            if False
-        :return: a Jim object
-        """
-        if not dx:
-            dx = 0
-        if not dy:
-            dy = 0
-        for band in bands:
-            self._jim_object.setData(value, ulx, uly, lrx, lry, band, dx, dy,
-                                     geo)
-
-    # def setData(self, value, bands=0):
-    #     """Set all pixels to value.
-
-    #     :param jim_object: a Jim object
-    #     :param value: new value for pixels of Jim object
-    #     :param band: List of band indices to crop (index is 0 based)
-    #     :return: a Jim object
-    #     """
-    #     for band in bands:
-    #         self._jim_object.setData(value,band)
 
     def convert(self, otype, **kwargs):
         """Convert Jim image with respect to data type.
@@ -218,6 +227,64 @@ class _PixOps():
         """
         kwargs.update({'otype': otype})
         self._jim_object._set(self._jim_object.convert(kwargs))
+
+    def isEqual(self, other):
+        if isinstance(other, _pj.Jim):
+            return self._jim_object.isEqual(other)
+        else:
+            return False
+
+    def NDVI(self, redBand, nirBand):
+        """Compute NDVI on the Jim object.
+
+        Modifies the instance on which the method was called.
+
+        :param redBand: index of band with values of red
+        :param nirBand: index of band with values of NIR
+        """
+        red = _pj.geometry.cropBand(self._jim_object, redBand)
+        nir = _pj.geometry.cropBand(self._jim_object, nirBand)
+
+        self._jim_object._set(nir.pointOpNDI(red))
+
+    def NDVISeparateBands(self, nirJim):
+        """Compute NDVI from two Jims (call on red band, use NIR as param).
+
+        Modifies the instance on which the method was called.
+
+        :param nirJim: Jim object with values of NIR
+        """
+        self._jim_object._set(nirJim.pointOpNDI(self._jim_object))
+
+    def setData(self, value, ulx=None, uly=None, lrx=None, lry=None, bands=[0],
+                dx=0, dy=0, geo=True):
+        """Set range of pixels to value.
+
+        :param jim_object: a Jim object
+        :param value: new value for pixels of Jim object
+        :param ulx: upper left corner x coordinate (in projected coordinates
+            if geo is True, else in image coordinates)
+        :param uly: upper left corner y coordinate (in projected coordinates
+            if geo is True, else in image coordinates)
+        :param lrx: lower right corner x coordinate (in projected coordinates
+            if geo is True, else in image coordinates)
+        :param lry: lower right corner y coordinate (in projected coordinates
+            if geo is True, else in image coordinates)
+        :param bands: List of band indices to crop (index is 0 based)
+        :param dx: spatial resolution in x to crop (stride if geo is False)
+        :param dy: spatial resolution in y to crop (stride if geo is False)
+        :param geo: use geospatial coordinates if True, image coordinates
+            if False
+        :return: a Jim object
+        """
+        if all(v is None for v in [ulx, uly, lrx, lry]) and bands==[0] and \
+                dx==0 and dy==0 and geo:
+            for band in bands:
+                self._jim_object.setData(value)
+        else:
+            for band in bands:
+                self._jim_object.setData(value, ulx, uly, lrx, lry, band, dx,
+                                         dy, geo)
 
     def setThreshold(self, **kwargs):
         """Apply min and max threshold to pixel values in raster dataset.
@@ -265,28 +332,6 @@ class _PixOps():
             jim_threshold=jim.setThreshold(min=0,max=250,nodata=255)
         """
         self._jim_object._set(self._jim_object.setThreshold(kwargs))
-
-    def NDVI(self, redBand, nirBand):
-        """Compute NDVI on the Jim object.
-
-        Modifies the instance on which the method was called.
-
-        :param redBand: index of band with values of red
-        :param nirBand: index of band with values of NIR
-        """
-        red = _pj.geometry.cropBand(self._jim_object, redBand)
-        nir = _pj.geometry.cropBand(self._jim_object, nirBand)
-
-        self._jim_object._set(nir.pointOpNDI(red))
-
-    def NDVISeparateBands(self, nirJim):
-        """Compute NDVI from two Jims (call on red band, use NIR as param).
-
-        Modifies the instance on which the method was called.
-
-        :param nirJim: Jim object with values of NIR
-        """
-        self._jim_object._set(nirJim.pointOpNDI(self._jim_object))
 
     def supremum(self, *args):
         """Change values of Jim for the biggest ones from provided Jim objects.
--- tests/test_pixops.py
+++ tests/test_pixops.py
@@ -8,8 +8,8 @@ testFile = 'tests/data/modis_ndvi_2010.tif'
 tiles = ['tests/data/red1.tif', 'tests/data/red2.tif']
 
 
-class BadMaxNDVI(unittest.TestCase):
-    """Test functions and methods needed to compute max NDVI."""
+class BadPixOps(unittest.TestCase):
+    """Test functions and methods from pixops modules."""
 
     def test_NDVI(self):
         """Test computing NDVI in different ways."""
@@ -56,10 +56,49 @@ class BadMaxNDVI(unittest.TestCase):
                 max_ndvi = pj.pixops.NDVISeparateBands(jim4, jim8)
                 max_ndvi2 = pj.Jim(max_ndvi)
 
+    def test_setFunctions(self):
+        """Test setData and setThreshold functions and methods."""
+        jim = pj.Jim(tiles[0])
+
+        stats = jim.stats.getStats()
+        min = stats['min']
+        max = stats['max']
+
+        thresholded = pj.pixops.setThreshold(jim, min=min+1, max=max-1,
+                                             nodata=(max+min)/2)
+
+        thresholded_stats = thresholded.stats.getStats()
+        nodata = thresholded.properties.getNoDataVals()
+
+        assert thresholded_stats != stats, \
+            'Error in pixops.setThreshold() or stats.getStats()'
+        assert thresholded_stats['min'] >= min + 1, \
+            'Error in pixops.setThreshold() or stats.getStats()'
+        assert thresholded_stats['max'] <= max - 1, \
+            'Error in pixops.setThreshold() or stats.getStats()'
+        assert thresholded.properties.getNoDataVals() == [((max+min)/2)], \
+            'Error in pixops.setThreshold() or properties.getNoDataVals()'
+
+        jim.pixops.setThreshold(min=min+1, max=max-1, nodata=(max+min)/2)
+
+        stats = jim.stats.getStats()
+
+        assert stats == thresholded_stats, \
+            'Error in pixops.setThreshold() or stats.getStats()'
+        assert jim.properties.getNoDataVals() == nodata, \
+            'Error in pixops.setThreshold() or properties.getNoDataVals()'
+
+        jim.setData(5)
+
+        stats = jim.stats.getStats()
+
+        assert all(v==5 for v in [stats['min'], stats['max'], stats['mean']]),\
+            'Error in pixops.setData() or stats.getStats()'
+
 
 def load_tests(loader=None, tests=None, pattern=None):
     """Load tests."""
     if not loader:
         loader = unittest.TestLoader()
-    suite_list = [loader.loadTestsFromTestCase(BadMaxNDVI)]
+    suite_list = [loader.loadTestsFromTestCase(BadPixOps)]
     return unittest.TestSuite(suite_list)
