d003e23d68cb366a6f1acc352dabe962b4580968
pesekon2 <pesej.ondrek@gmail.com>
2019-04-18 17:10:43 +0200

    make stats.getStats() more optimal for methods and JimLists

--- pyjeo/modules/stats.py
+++ pyjeo/modules/stats.py
@@ -5,8 +5,29 @@ import numpy
 
 
 def getStats(jim_object, function=['min','max','mean'], **kwargs):
-    kwargs.update({'function': function})
-    return jim_object._jipjim.getStats(kwargs)
+    if not isinstance(function, list):
+        function = function.split(',')
+
+    statDict = dict()
+    if 'min' in function:
+        statDict['min'] = numpy.min(jim_object.np()).item()
+    if 'max' in function:
+        statDict['max'] = numpy.max(jim_object.np()).item()
+    if 'mean' in function:
+        statDict['mean'] = numpy.mean(jim_object.np()).item()
+    if 'median' in function:
+        statDict['median'] = numpy.median(jim_object.np()).item()
+
+    for f in function:
+        if f not in ['min','max','mean','median']:
+            kwargs.update({'function': f})
+
+    if kwargs:
+        statDict.update(jim_object.getStats(kwargs))
+
+    return statDict
+    # kwargs.update({'function': function})
+    # return jim_object._jipjim.getStats(kwargs)
 
 
 def getStatProfile(jim_object, function, **kwargs):
@@ -106,23 +127,26 @@ class _Stats():
         return _pj.Jim(self._jim_object._jipjim.histo1dCumulative())
 
     def getStats(self, function=['min','max','mean'], **kwargs):
-        if not isinstance(function,list):
-            function=[function]
-        statDict={}
+        if not isinstance(function, list):
+            function = function.split(',')
+
+        statDict = dict()
         if 'min' in function:
-            statDict['min']=numpy.min(self._jim_object.np()).item()
+            statDict['min'] = numpy.min(self._jim_object.np()).item()
         if 'max' in function:
-            statDict['max']=numpy.max(self._jim_object.np()).item()
+            statDict['max'] = numpy.max(self._jim_object.np()).item()
         if 'mean' in function:
-            statDict['mean']=numpy.mean(self._jim_object.np()).item()
+            statDict['mean'] = numpy.mean(self._jim_object.np()).item()
         if 'median' in function:
-            statDict['median']=numpy.median(self._jim_object.np()).item()
+            statDict['median'] = numpy.median(self._jim_object.np()).item()
 
         for f in function:
             if f not in ['min','max','mean','median']:
                 kwargs.update({'function': f})
-            if kwargs:
-                statDict.update(self._jim_object._jipjim.getStats(kwargs))
+
+        if kwargs:
+            statDict.update(self._jim_object._jipjim.getStats(kwargs))
+
         return statDict
 
     def statProfile(self, function, **kwargs):
@@ -148,8 +172,30 @@ class _StatsList():
         return _pj.Jim(self._jim_list._jipjimlist.statProfile(kwargs))
 
     def getStats(self, function=['min','max','mean'], **kwargs):
-        kwargs.update({'function': function})
-        return self._jim_list._jipjimlist.getStats(kwargs)
+        if not isinstance(function, list):
+            function = function.split(',')
+
+        statDict = {'min': None, 'max': None}
+        for item in self._jim_list:
+            if 'min' in function:
+                min = numpy.min(item.np()).item()
+                if not statDict['min'] or statDict['min'] > min:
+                    statDict['min'] = min
+            if 'max' in function:
+                max = numpy.max(item.np()).item()
+                if max > statDict['max']:
+                    statDict['max'] = max
+
+        for f in function:
+            if f not in ['min', 'max']:
+                kwargs.update({'function': f})
+
+        if kwargs:
+            statDict.update(self._jim_list._jipjimlist.getStats(kwargs))
+
+        return statDict
+        # kwargs.update({'function': function})
+        # return self._jim_list._jipjimlist.getStats(kwargs)
 
 
 class _StatsVect():
