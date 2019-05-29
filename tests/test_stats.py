d967c78f9fa3a677811d94e7263518916f9a2d2d
pesekon2 <pesej.ondrek@gmail.com>
2019-05-29 14:33:34 +0200

    add more tests for Jim.stats.getStats()

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -34,8 +34,21 @@ class BadStats(unittest.TestCase):
         assert min < mean < max, \
             'Error in getting statistics with stats.getStats()'
 
+        jim.properties.clearNoData()
+
+        stats = jim.stats.getStats('median,invalid')
+        stats2 = pj.stats.getStats(jim, 'median,invalid')
+
+        assert stats == stats2, 'Inconsistency in getStats() (method returns' \
+                                ' different result than function)'
+
+        assert stats['ninvalid'] == 0, \
+            'Error in properties.clearNoData() or ' \
+            'getStats(function="invalid") (nodata detected after clearing)'
+
         stats = jim.stats.getStats(['max', 'median', 'invalid'], nodata=max)
-        stats2 = pj.stats.getStats(jim, ['max', 'median'], nodata=max)
+        stats2 = pj.stats.getStats(jim, ['max', 'median', 'invalid'],
+                                   nodata=max)
 
         assert stats == stats2, 'Inconsistency in getStats() (method returns' \
                                 ' different result than function)'
@@ -46,6 +59,9 @@ class BadStats(unittest.TestCase):
         assert stats['max'] > stats['median'] > min, \
             'Suspicious value of median with getStats() ' \
             '(not smaller than max or not bigger than min)'
+
+        assert stats['ninvalid'] > 0, 'Error in getStats(function="invalid")' \
+                                      ' (no nodata detected, but should be)'
 
     def test_histograms(self):
         """Test that values of histograms are not suspicious."""
