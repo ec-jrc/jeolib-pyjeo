27709a9ac8f495492ad93de5505515f20a92ec34
pesekon2 <pesej.ondrek@gmail.com>
2019-08-06 14:36:01 +0200

    add tests for creation of random Jim

--- tests/test_jim_basics.py
+++ tests/test_jim_basics.py
@@ -57,6 +57,35 @@ class BadBasicMethods(unittest.TestCase):
         assert jim7.pixops.isEqual(jim8), \
             'Error in usage of seed keyword argument when creating Jim()'
 
+        jim8_2 = pj.Jim(nrow=500, ncol=500, otype='Float32', uniform=2,
+                        seed=0)
+
+        assert jim7.pixops.isEqual(jim8_2), \
+            'Error in using only one value for uniform creation of Jim()' \
+            '(created Jim is not the same as the one created with[0, value])'
+
+        try:
+            _ = pj.Jim(nrow=5, ncol=5, otype='Float32', uniform=[0, 0, 0])
+            failed = True
+        except AttributeError:
+            failed = False
+
+        assert not failed, \
+            'Error in catching a call of Jim creation with wrong value ' \
+            'parsed as the uniform argument (three values parsed)'
+
+        jim9 = pj.Jim(nrow=500, ncol=500, otype='Float32', mean=500, stdev=50,
+                      seed=0)
+
+        stats = jim9.stats.getStats(['mean', 'stdev'])
+
+        assert 500 - 50 < stats['mean'] < 500 + 50, \
+            'Suspicious values in Jim when creating it with Jim(mean, stdev)' \
+            '(mean seems too distant from the specified one)'
+        assert 50 - 5 < stats['stdev'] < 50 + 5, \
+            'Suspicious values in Jim when creating it with Jim(mean, stdev)' \
+            '(stdev seems too different from the specified one)'
+
         try:
             _ = pj.Jim(seed=5)
             failed = True
