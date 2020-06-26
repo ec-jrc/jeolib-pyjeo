e420e87d6bedccc40e1b28640058458675c289a1
Pieter Kempeneers <pieter.kempeneers@ec.europa.eu>
2020-06-26 16:28:50 +0200

    setting license to GPLv3

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ JRC Ispra.
 ## License
 
 pyJEO is released under an
-[EUPL](https://joinup.ec.europa.eu/collection/eupl) license (see
+[GPLv3](http://www.gnu.org/licenses/gpl-3.0.html) license (see
 [LICENSE.txt](LICENSE.txt))
 
 ## Dependencies
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author_email='ondrej.pesek@ext.ec.europa.eu',
     url='https://cidportal.jrc.ec.europa.eu/apps/gitlab/JIPlib/pyJEO',
     description='https://jeodpp.jrc.ec.europa.eu/services/processing/pyjeohelp',
-    license='EUPL',
+    license='GPLv3',
     packages=find_packages(exclude=['doc', 'tests']),
     include_package_data=True,
     install_requires='numpy'
