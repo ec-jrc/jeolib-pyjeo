b69732c47dc676b8820c81151e747e237e2cf5ab
Pieter Kempeneers <pieter.kempeneers@ec.europa.eu>
2020-06-26 16:28:50 +0200

    setting license to GPLv3

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ from setuptools import find_packages
 setup(
     name='pyjeo',
     version='0.6.0',
-    author_email='ondrej.pesek@ext.ec.europa.eu',
+    author_email='pieter.kempeneers@.ec.europa.eu',
     url='https://cidportal.jrc.ec.europa.eu/apps/gitlab/JIPlib/pyJEO',
     description='https://jeodpp.jrc.ec.europa.eu/services/processing/pyjeohelp',
-    license='EUPL',
+    license='GPLv3',
     packages=find_packages(exclude=['doc', 'tests']),
     include_package_data=True,
     install_requires='numpy'
