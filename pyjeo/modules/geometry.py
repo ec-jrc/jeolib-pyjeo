1a45e4016e52b6816335946df97c3b938dc229f3
ondrej <pesej.ondrek@gmail.com>
2020-08-17 13:15:00 +0200

    fix JimList does not have an attribute _jim_list
    fix #54

--- pyjeo/modules/geometry.py
+++ pyjeo/modules/geometry.py
@@ -2821,7 +2821,7 @@ class _GeometryVect(_pj.modules.JimVectModuleBase):
             #todo: support multi-band images in JimList...
             if bandname is None:
                 bandname = [
-                    't'+str(ifile) for ifile in range(0, len(jim._jim_list))]
+                    't'+str(ifile) for ifile in range(0, len(jim._jipjimlist))]
             kwargs.update({'bandname': bandname})
 
             avect = jim._jipjimlist.extractOgr(self._jim_vect._jipjimvect,
