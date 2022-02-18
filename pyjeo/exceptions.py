467dc5d44c5e5f8a5c81a7114bff34c09b0fe003
Pieter Kempeneers <pieter.kempeneers@ec.europa.eu>
2022-02-18 10:40:46 +0100

    copyright -> 2022

--- pyjeo/exceptions.py
+++ pyjeo/exceptions.py
@@ -3,7 +3,7 @@
 #            Ondrej Pesek,
 #            Pierre.Soille@ec.europa.eu
 #
-# Copyright (C) 2018-2020 European Union (Joint Research Centre)
+# Copyright (C) 2018-2022 European Union (Joint Research Centre)
 #
 # This file is part of pyjeo.
 #
