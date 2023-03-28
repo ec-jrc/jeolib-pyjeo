0cfefcfffbe3ac17a7026766b3d7658b53a6b32c
Pieter Kempeneers <pieter.kempeneers@ec.europa.eu>
2023-03-28 15:52:16 +0200

    testing xr2jim

--- pyjeo/pyjeo.py
+++ pyjeo/pyjeo.py
@@ -21,13 +21,15 @@
 # along with pyjeo.  If not, see <https://www.gnu.org/licenses/>.
 
 from __future__ import division
+import math
 import copy
 import numpy as _np
 import gc as _gc
 import warnings as _warnings
 import os as _os
+from rasterio import crs as _crs
+from pandas import to_datetime
 from datetime import time, timedelta, datetime
-import math
 from pathlib import Path
 from osgeo import ogr as _ogr
 import xarray as _xr
@@ -1831,7 +1833,6 @@ def np2jim(np_object: _np.ndarray) -> Jim:
     """
     return Jim(_jl.np2jim(np_object))
 
-
 def xr2jim(xr_object) -> Jim:
     """Return a Jim representation from an xarray.
 
@@ -1847,10 +1848,96 @@ def xr2jim(xr_object) -> Jim:
     :param xr_object: an xarray
     :return: a Jim representation from  an xarray
     """
-    import xarray as _xr
-    import rioxarray
-    from rasterio import crs as _crs
-    from pandas import to_datetime
+    assert xr_object.sizes.get('y') is not None
+    assert xr_object.sizes.get('x') is not None
+
+    jim = None
+    projection = None
+    for b in xr_object:
+        if xr_object[b].attrs.get('crs_wkt') is not None:
+            projection = xr_object[b].attrs.get('crs_wkt')
+        elif xr_object[b].attrs.get('spatial_ref') is not None:
+            #for backward compatibility
+            projection = xr_object[b].attrs.get('spatial_ref')
+        elif jim is None:
+            jim = np2jim(xr_object[b].values)
+            #seems redundant...
+            jim.np(0)[:] = xr_object[b].values
+            gt = []
+            try:
+                dx = xr_object[b].coords['x'].values[1]-xr_object[b].coords['x'].values[0]
+                dy = xr_object[b].coords['y'].values[0]-xr_object[b].coords['y'].values[1]
+                ulx = xr_object[b].coords['x'].values[0]-dx/2.0
+                uly = xr_object[b].coords['y'].values[0]+dy/2.0
+            except KeyError:
+                dx = xr_object[b].coords['lon'].values[1]-xr_object[b].coords['lon'].values[0]
+                dy = xr_object[b].coords['lat'].values[0]-xr_object[b].coords['lat'].values[1]
+                ulx = xr_object[b].coords['lon'].values[0]-dx/2.0
+                uly = xr_object[b].coords['lat'].values[0]+dy/2.0
+            gt.append(ulx)
+            gt.append(dx)
+            gt.append(0)
+            gt.append(uly)
+            gt.append(0)
+            gt.append(-dy)
+            jim.properties.setGeoTransform(gt)
+        else:
+            if len(xr_object[b].values.shape) > 2:
+                assert xr_object[b].values.shape[0] == jim.properties.nrOfPlane() ,\
+                    str("Error: number of planes is not consistent: {} != {}".format(xr_object[b].values.shape[0], jim.properties.nrOfPlane()))
+                assert xr_object[b].values.shape[1] == jim.properties.nrOfRow() ,\
+                    str("Error: number of rows is not consistent: {} != {}".format(xr_object[b].values.shape[1], jim.properties.nrOfRow()))
+                assert xr_object[b].values.shape[2] == jim.properties.nrOfCol() ,\
+                    str("Error: number of cols is not consistent: {} != {}".format(xr_object[b].values.shape[2], jim.properties.nrOfCol()))
+            else:
+                assert xr_object[b].values.shape[0] == jim.properties.nrOfRow() ,\
+                    str("Error: number of rows is not consistent: {} != {}".format(xr_object[b].values.shape[0], jim.properties.nrOfRow()))
+                assert xr_object[b].values.shape[1] == jim.properties.nrOfCol() ,\
+                    str("Error: number of cols is not consistent: {} != {}".format(xr_object[b].values.shape[1], jim.properties.nrOfCol()))
+            jim.geometry.stackBand(np2jim(xr_object[b].values))
+            #seems redundant...
+            jim.np(-1)[:] = xr_object[b].values
+    if xr_object.coords.get('time') is not None:
+        planes = to_datetime(xr_object.time.data).to_pydatetime()
+        if isinstance(planes, _np.ndarray):
+            jim.properties.setDimension(planes.tolist(), 'plane')
+        else:
+            jim.properties.setDimension(planes, 'plane')
+
+    bands = []
+    if isinstance(xr_object, _xr.Dataset):
+        for band in xr_object.data_vars:
+            if band != 'spatial_ref':
+                bands.append(band)
+    else:
+        print("Warning: could not get band name from DataArray, setting 0")
+        bands.append(str(0))
+    jim.properties.setDimension(bands, 'band')
+    jim.properties.setGeoTransform(gt)
+    if projection is not None:
+        jim.properties.setProjection(projection)
+
+    return jim
+
+def xr2jimnew(xr_object) -> Jim:
+    """Return a Jim representation from an xarray.
+
+    The (contiguous) data array must be organized
+    as [planes][rows][columns] for the respective bands
+
+    Returns a 3D Jim object with planes and bands
+    xarray must be a cube with identical x and y coordinates for each
+    dataset
+    Notice the (contiguous) data array is organized as
+    [planes][rows][columns] for the respective bands
+
+    :param xr_object: an xarray
+    :return: a Jim representation from  an xarray
+    """
+    # import xarray as _xr
+    # import rioxarray
+    # from rasterio import crs as _crs
+    # from pandas import to_datetime
 
     assert xr_object.sizes.get('y') is not None
     assert xr_object.sizes.get('x') is not None
