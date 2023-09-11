d349b86e5ddeaee74cf72c936eafe398c8e6103f
Pieter Kempeneers <pieter.kempeneers@ec.europa.eu>
2023-09-11 16:25:38 +0200

    add dependencies in pyproject.toml

--- doc/conf.py
+++ doc/conf.py
@@ -48,7 +48,7 @@ extensions = ['sphinx.ext.autodoc',
 # Add path to bibtex file
 bibtex_bibfiles = ['./references.bib']
 
-autodoc_mock_imports = ['jiplib', 'numpy', 'osgeo', 'rasterio', 'pandas', 'xarray', 'rioxarray', 'netCDF4']
+autodoc_mock_imports = ['jiplib', 'numpy', 'rasterio', 'pandas', 'xarray', 'rioxarray', 'netCDF4', 'pyproj', 'shapely']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
