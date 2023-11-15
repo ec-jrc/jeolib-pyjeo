f53612d2ae6cfa8cc7bb04ba5f9309b1ffb66c24
Pieter Kempeneers <kempepi@maggie.arolo>
2023-11-15 11:47:22 +0100

    rasterfn -> testFile

--- tests/test_jim_basics.py
+++ tests/test_jim_basics.py
@@ -1431,7 +1431,7 @@ class BadBasicMethods(unittest.TestCase):
             datetime.strptime('2019-01-01','%Y-%m-%d'),
             datetime.strptime('2019-01-05','%Y-%m-%d')]
         bands = ['B0', 'B1']
-
+ 
         jim.properties.setDimension({'plane' : dates[0:1],
                                     'band' : bands})
         x = pj.jim2xr(jim)
@@ -1444,7 +1444,7 @@ class BadBasicMethods(unittest.TestCase):
             'Inconsistency in bridging to xarray single plane ' \
             '(pj.jim2xr() != jim.xr(0) is identical)'
 
-        jim.geometry.stackPlane(pj.Jim(rasterfn, band=[2, 3]))
+        jim.geometry.stackPlane(pj.Jim(testFile, band=[2, 3]))
         jim.properties.setDimension({'plane' : dates,
                                     'band' : bands})
 
